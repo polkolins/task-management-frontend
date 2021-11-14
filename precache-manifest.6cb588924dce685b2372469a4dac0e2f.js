26872f69abee67c64176e9a7779c0d92676c81cb
polkolins <polkolins@users.noreply.github.com>
2021-11-14 16:16:52 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.6cb588924dce685b2372469a4dac0e2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43063cce85f51c5aa4d91faeeeaa752",
+    "url": "https://polkolins.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "99bae729ed803d7d3aa8",
+    "url": "https://polkolins.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "e85ea2ebfa58a277a3fa",
+    "url": "https://polkolins.github.io/task-management-frontend/static/js/2.87d16b0c.chunk.js"
+  },
+  {
+    "revision": "99bae729ed803d7d3aa8",
+    "url": "https://polkolins.github.io/task-management-frontend/static/js/main.e935c744.chunk.js"
+  },
+  {
+    "revision": "7ff6ebda720b078ded23",
+    "url": "https://polkolins.github.io/task-management-frontend/static/js/runtime~main.775ffca8.js"
+  }
+]);
